5714cc44cda5599de75694d46a6fff26a1f1c7e1
mitulpatil96 <mitulpatil0309@gmail.com>
2026-08-20 12:51:24 +0530

    dsa 3rd

---
+++ mits dsa3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	float base,height,area;
+	printf("enter the base ");
+	scanf("%f",&base);
+	printf("enter the height ");
+	scanf("%f",&height);
+	
+	area =0.5*base*height;
+	printf("area of tiangle=%2f\n",area);
+	return 0;
+}
+
